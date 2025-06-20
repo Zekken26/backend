b56ce27eccd80e6f3e28d69cdf21cdc928388e34
Zekken26 <kiangutierrez02@gmail.com>
2025-06-20 14:38:24 +0800

    Prepare for Render deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1c0%moo=4#arobz3ded0^k%njil&lg5_lhp7i^7mvvbkry8)rs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
